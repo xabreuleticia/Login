0153e46fa92166160d5d2fe720b41f5d6e8c37ce
Leticia Abreu <xabreu.leticia@gmail.com>
2026-09-03 12:21:29 -0300

    Add files via upload

---
+++ LoginPUC/src/main/java/com/example/LoginPUC/LoginPucApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoginPUC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPucApplication.class, args);
+	}
+
+}
