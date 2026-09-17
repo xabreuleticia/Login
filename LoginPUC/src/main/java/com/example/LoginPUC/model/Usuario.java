6ccb2828721f9f2ff08802accba7522010b175dc
Letícia Xavier Abreu <1553571@sga.pucminas.br>
2026-09-17 15:14:55 -0300

    Verificação das informações do cadastro

---
+++ LoginPUC/src/main/java/com/example/LoginPUC/model/Usuario.java
@@ -0,0 +1,26 @@
+package com.example.LoginPUC.model;
+
+public class Usuario {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public Usuario(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
