8a064ebd472ea9c6d5e413deaa0afb9cc9582f90
Leticia Abreu <xabreu.leticia@gmail.com>
2026-09-17 23:14:03 -0300

    Correção

--- LoginPUC/src/main/java/com/example/LoginPUC/controller/LoginController.java
+++ LoginPUC/src/main/java/com/example/LoginPUC/controller/LoginController.java
@@ -16,12 +16,6 @@ public class LoginController {
 
     private List<Usuario> usuarios = new ArrayList<>();
 
-    private final EmailService emailService;
-
-    public LoginController(EmailService emailService) {
-        this.emailService = emailService;
-    }
-
     @GetMapping("/")
     public String inicio() {
         return "login";
